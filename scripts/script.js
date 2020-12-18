f1f30237c9afdd85f81cdbe7b9e67a1618a8790e
Nirmal Mekala <ndmekala@icloud.com>
2020-12-18 16:29:37 -0600

    added functionality with DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 
+// creat playerScore and computerScore variables
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 // Function that randomly returns rock, paper, or scissors
 
@@ -15,53 +20,102 @@ function computerPlay() {
     };
 };
 
-// Add an event listener to the buttons that calsl your playRound function with the correct playerSelection every time abutton is clicked
-
-// button triggers playRound with playerSelection
-
-// Function that solicits and returns a player choice (and chooses rock if the player doesn’t input a legitimate choice)
-
-function getUserInput() {
-    let userInputInsensitive = prompt("Rock, paper, scissors! What is your choice?", "");
-    userInput = userInputInsensitive.toLowerCase();
-    if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
-        return userInput;
-    } else {
-        alert('That’s not an option! (It’s called “Rock, paper, scissors”, silly!) Rock it is!');
-        return "rock";
-    }
-}
-
 // FUNCTION single round of Rock/paper/scissors with the parameters being strings of rock, paper, or scissors
 
 function playRound(player, computer) {
     if (player == computer){
-        return "It’s a Draw!";
+        return 1;
     } else if (player == "rock"){
         if (computer == "paper") {
-            return "You lose! Paper covers rock!";
+            return 2;
         } else if (computer == "scissors") {
-            return "You win! Rock beats scissors!";
+            return 3;
         };
     } else if (player == "paper"){
         if (computer == "scissors") {
-            return "You lose! Scissors cuts paper!";
+            return 2;
         } else if (computer == "rock") {
-            return "You win! Paper covers rock!";
+            return 3;
         };
     } else if (player == "scissors"){
         if (computer == "rock") {
-            return "You lose! Rock beats scissors!";
+            return 2;
         } else if (computer == "paper") {
-            return "You win! Scissors cuts paper!";
+            return 3;
         };
     }
 }
+
+// button triggers playRound with playerSelection
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        console.log(button.id)
+        playerSelection = button.id;
+        computerSelection = computerPlay();
+        result = playRound(playerSelection,computerSelection);
+        tally(result);
+    })
+})
+
+// tally function: keep track of variables and 
+
+function tally(gameResult) {
+    // if (playerScore && 
+
+    const resultsContainer = document.querySelector('#results');
+    resultsContainer.style.cssText = "background-color: pink; text-align: left;"
+
+
+    if (gameResult === 3) {
+        playerScore++;
+        resultsContainer.textContent = 'Computer: ' + computerScore + ' Player: ' + playerScore;
+    } else if (gameResult === 2) {
+        computerScore++;
+        resultsContainer.textContent = 'Computer: ' + computerScore + ' Player: ' + playerScore;
+    } else if (gameResult === 1) {
+        resultsContainer.textContent = 'Computer: ' + computerScore + ' Player: ' + playerScore + " (Draw)";
+    } else {
+        alert ('Something’s gone terribly wrong!');
+    };
+    if (playerScore >= 5) {
+        alert('You win! ' + 'Computer: ' + computerScore + ' Player: ' + playerScore);
+        resultsContainer.textContent = 'Computer: ' + computerScore + ' Player: ' + playerScore;
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore >=5) {
+        alert('You lose! ' + 'Computer: ' + computerScore + ' Player: ' + playerScore);
+        resultsContainer.textContent = 'Computer: ' + computerScore + ' Player: ' + playerScore;
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+    };
+}
+
+
+// Function that solicits and returns a player choice (and chooses rock if the player doesn’t input a legitimate choice)
+
+// function getUserInput(input) {
+//     let userInputInsensitive = input;
+//     userInput = userInputInsensitive.toLowerCase();
+//     if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
+//         return userInput;
+//     } else {
+//         alert('That’s not an option! (It’s called “Rock, paper, scissors”, silly!) Rock it is!');
+//         return "rock";
+//     }
+// }
 
 // To juice this up, you’d need to figure out a way to add enough iterations to get a result when you have a draw
 // Another way to improve it would be to evaluate after games 3, 4, and 5 whether either computer or player had won enough games to win the “series”
 // I only built it with the playRound function outputtng a string because it said that initially.
 // I didn’t read that we could rework the functions. I would have had it input a number or something to make the if function simpler
+
+
+
+
+
+
 
 // Game function: five rounds, displaying results after each round and alerting the winner at the end
 function game() {
@@ -152,5 +206,3 @@ function game() {
     userCount = 0;
     computerCount = 0;
 }
-
-game();
